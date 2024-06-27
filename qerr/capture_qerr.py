9d6ec65dd75bc454f85139ea0eeebb48e81cf53f
Nicolas Rault-Wang <nraultwang@berkeley.edu>
2024-06-27 16:32:57 -0700

    Add data fields for NAV-TIMEUTC packets.

--- qerr/capture_qerr.py
+++ qerr/capture_qerr.py
@@ -14,25 +14,6 @@ from pyubx2 import UBXReader, UBX_PROTOCOL, UBXMessage, SET_LAYER_RAM, POLL_LAYE
 qerr_config_file = 'qerr_config.json'
 packet_data_dir = 'data'
 
-def create_empty_df():
-    df = pd.DataFrame(
-        columns=[
-            #
-            # TIM-TP data
-            'towMS (ms)',  # towMS (unit: ms)
-             'towSubMS',  # towSubMS (unit: ms, scale: 2^-32)
-             'qErr (ps)',  # qErr (unit: ps)
-             'week (weeks)',  # week (unit: weeks)
-             'timeBase_flag',
-             'utc_flag',
-             'raim_flag',
-             'qErrInvalid_flag',
-             'timeRefGnss'
-        ]
-    )
-    return df
-
-
 
 
 def get_experiment_fname(start_timestamp):
@@ -62,13 +43,15 @@ def make_json_config():
             'receiver': [
                 {
                     'name': '...',
-                    'device': '/dev/...',
+                    'device_rpi': '/dev/...',
+                    'device_wsl': '/dev/...',
                     'baudrate': 38400,
                     'comments': '...'
                 },
                 {
                     'name': '...',
-                    'device': '/dev/...',
+                    'device_rpi': '/dev/...',
+                    'device_wsl': '/dev/...',
                     'baudrate': 38400,
                     'comments': '...'
                 }
@@ -82,6 +65,38 @@ def make_json_config():
             print(f"{qerr_config_file} already exists")
 
 
+def create_empty_df():
+    df = pd.DataFrame(
+        columns=[
+            # NAV-TIMEUTC data
+            'iTOW (ms)',
+            'tAcc (ns)',
+            'year',
+            'month',
+            'day',
+            'hour',
+            'min',
+            'sec',
+            'validTOW_flag',
+            'validWKN_flag',
+            'validUTC_flag',
+            'utcStandard_NAV-TIMEUTC'
+            # TIM-TP data
+            'towMS (ms)',  # towMS (unit: ms)
+            'towSubMS',  # towSubMS (unit: ms, scale: 2^-32)
+            'qErr (ps)',  # qErr (unit: ps)
+            'week (weeks)',  # week (unit: weeks)
+            'timeBase_flag',
+            'utc_flag',
+            'raim_flag',
+            'qErrInvalid_flag',
+            'timeRefGnss',
+            'utcStandard_TIM-TP'
+        ]
+    )
+    return df
+
+
 def collect_data(df, qerr_config, timeout=10):
     device = qerr_config['device']
     baudrate = qerr_config['baudrate']
@@ -89,12 +104,12 @@ def collect_data(df, qerr_config, timeout=10):
         ubr = UBXReader(stream, protfilter=UBX_PROTOCOL)
         # Save packets and timestamp their arrival.
         packet_cache = {
-            'TIM-TP': {
+            'NAV-TIMEUTC': {
                 'valid': False,
                 'parsed_data': None,
                 'timestamp': None
             },
-            'NAV-TIMEUTC': {
+            'TIM-TP': {
                 'valid': False,
                 'parsed_data': None,
                 'timestamp': None
@@ -103,58 +118,67 @@ def collect_data(df, qerr_config, timeout=10):
         while True:
             # Check if packet cache is full.
             if packet_cache['TIM-TP']['valid'] and packet_cache['NAV-TIMEUTC']['valid']:
-                if packet_cache['TIM-TP']['timestamp']:
-                    ...
-                    # TODO: verify that packet timestamps differ by no more than 1s.
-                    # If packets do differ by more than 1s, write the earlier packet, and fill remaining data with NULLS?
-                merged_data = {
-                    **packet_cache['TIM-TP']['parsed_data'],
-                    **packet_cache['NAV-TIMEUTC']['parsed_data']
-                }
-                if set(merged_data.keys()) != set(df.columns.tolist()):
-                    raise KeyError(
-                        'packet keys do not match data schema:\nPacket keys: {}\nSchema: {}'.format(
-                            list(merged_data.keys()), df.columns.tolist()
+                # Verify that packet timestamps differ by no more than 1s.
+                time_diff = abs(packet_cache['TIM-TP']['timestamp'] - packet_cache['NAV-TIMEUTC']['timestamp'])
+                microsec_time_diff = time_diff.seconds * 1e6 + time_diff.microseconds
+                if microsec_time_diff < 1e6:
+                    # Merge packet data into a single dict
+                    merged_data = {
+                        **packet_cache['TIM-TP']['parsed_data'],
+                        **packet_cache['NAV-TIMEUTC']['parsed_data']
+                    }
+                    if set(merged_data.keys()) != set(df.columns.tolist()):
+                        raise KeyError(
+                            'packet keys do not match data schema:\nPacket keys: {}\nSchema: {}'.format(
+                                list(merged_data.keys()), df.columns.tolist()
+                            )
                         )
-                    )
-                # Do write transaction
-                df.loc[len(df)] = merged_data
-
-                # Reset cache
-                packet_cache['TIM-TP']['valid'] = False
-                packet_cache['NAV-TIMEUTC']['valid'] = False
-
+                    # Do write transaction
+                    df.loc[len(df)] = merged_data
+                    # Reset cache
+                    packet_cache['TIM-TP']['valid'] = False
+                    packet_cache['NAV-TIMEUTC']['valid'] = False
+                else:
+                    # Drop the earlier packet.
+                    if packet_cache['TIM-TP']['timestamp'] < packet_cache['NAV-TIMEUTC']['timestamp']:
+                        packet_cache['TIM-TP']['valid'] = False
+                    else:
+                        packet_cache['NAV-TIMEUTC']['valid'] = False
+            # Wait for next packet
             raw_data, parsed_data = ubr.read()
             if parsed_data:
                 if parsed_data.identity == 'NAV-TIMEUTC':
                     # UBX-NAV-TIMEUTC
                     packet_cache['NAV-TIMEUTC']['timestamp'] = datetime.datetime.now()
-                    nav_time_utc_data = {
-                        'towMS (ms)': parsed_data.towMS,
-                        'towSubMS': parsed_data.towSubMS,
-                        'qErr (ps)': parsed_data.qErr,
-                        'week (weeks)': parsed_data.week,
-                        'timeBase_flag': parsed_data.timeBase,
-                        'utc_flag': parsed_data.utc,
-                        'raim_flag': parsed_data.raim,
-                        'qErrInvalid_flag': parsed_data.qErrInvalid,
-                        'timeRefGnss': parsed_data.timeRefGnss,
+                    packet_cache['NAV-TIMEUTC']['parsed_data'] = {
+                        'iTOW (ms)':        parsed_data.iTOW,
+                        'tAcc (ns)':        parsed_data.tAcc,
+                        'year':             parsed_data.year,
+                        'month':            parsed_data.month,
+                        'day':              parsed_data.day,
+                        'hour':             parsed_data.hour,
+                        'min':              parsed_data.min,
+                        'sec':              parsed_data.sec,
+                        'validTOW_flag':    parsed_data.validTOW,
+                        'validWKN_flag':    parsed_data.validWKN,
+                        'validUTC_flag':    parsed_data.validUTC,
+                        'utcStandard_NAV-TIMEUTC':  parsed_data.utcStandard
                     }
                 elif parsed_data.identity == 'TIM-TP':
                     # UBX-TIM-TP
                     packet_cache['TIM-TP']['timestamp'] = datetime.datetime.now()
                     packet_cache['TIM-TP']['parsed_data'] = {
-                        'towMS (ms)':       parsed_data.towMS,
-                        'towSubMS':         parsed_data.towSubMS,
-                        'qErr (ps)':        parsed_data.qErr,
-                        'week (weeks)':     parsed_data.week,
-                        'timeBase_flag':    parsed_data.timeBase,
-                        'utc_flag':         parsed_data.utc,
-                        'raim_flag':        parsed_data.raim,
-                        'qErrInvalid_flag': parsed_data.qErrInvalid,
-                        'timeRefGnss':      parsed_data.timeRefGnss,
+                        'towMS (ms)':           parsed_data.towMS,
+                        'towSubMS':             parsed_data.towSubMS,
+                        'qErr (ps)':            parsed_data.qErr,
+                        'week (weeks)':         parsed_data.week,
+                        'timeBase_flag':        parsed_data.timeBase,
+                        'utc_flag':             parsed_data.utc,
+                        'raim_flag':            parsed_data.raim,
+                        'qErrInvalid_flag':     parsed_data.qErrInvalid,
+                        'timeRefGnss':          parsed_data.timeRefGnss,
+                        'utcStandard_TIM-TP':   parsed_data.utcStandard
                     }
-
 
 def start():
     qerr_config = load_qerr_config()
